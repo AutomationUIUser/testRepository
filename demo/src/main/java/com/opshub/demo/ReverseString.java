ed10066002d73ed09d0aa71d50f4c4accf158cb2
Aakash251 <99273659+Aakash251@users.noreply.github.com>
2022-02-10 18:35:47 +0530

    ReverseString.java added for ODP-2479

---
+++ demo/src/main/java/com/opshub/demo/ReverseString.java
@@ -0,0 +1,22 @@
+package com.opshub.demo;
+
+import java.io.*;
+import java.util.Scanner;
+ 
+class ReverseString {
+    public static void main (String[] args) {
+       
+        String str= "Demo", nstr="";
+        char ch;
+       
+      System.out.print("Original word: ");
+      System.out.println("Demo");
+       
+      for (int i=0; i<str.length(); i++)
+      {
+        ch= str.charAt(i);
+        nstr= ch+nstr; 
+      }
+      System.out.println("Reversed word: "+ nstr);
+    }
+}
